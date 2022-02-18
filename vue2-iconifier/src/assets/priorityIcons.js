79820c7db2a399aa0d1857a447369ef9a6c33373
Celil Yiğit <70552170+Frostbite99@users.noreply.github.com>
2022-02-18 10:37:12 +0200

    First Commit

---
+++ vue2-iconifier/src/assets/priorityIcons.js
@@ -0,0 +1,188 @@
+const priority_icons = [
+    {
+      name: "taxi_alert",
+      value: "ef74",
+      turkish: "taksi2",
+    },
+    {
+      name: "restaurant",
+      value: "e56c",
+      turkish: "restoran",
+    },
+    {
+      name: "sports_bar",
+      value: "f1f3",
+      turkish: "bar",
+    },
+    {
+      name: "wine_bar",
+      value: "f1e8",
+      turkish: "bar2"
+    },
+    {
+      name: "school",
+      value: "e80c",
+      turkish: "okul",
+    },
+    {
+      name: "museum",
+      value: "ea36",
+      turkish: "müze",
+    },
+    {
+      name: "child_care",
+      value: "eb41",
+      turkish: "çocuk"
+    },
+    {
+      name: "local_airport",
+      value: "e53d",
+      turkish: "havaalanı"
+    },
+    {
+      name: "local_atm",
+      value: "e53e",
+      turkish: "atm",
+    },
+    {
+      name: "local_bar",
+      value: "e540",
+      turkish: "bar3"
+    },
+    {
+      name: "local_cafe",
+      value: "e541",
+      turkish:"kafe",
+    },
+    {
+      name: "local_car_wash",
+      value: "e542",
+      turkish:"araba yikama"
+    },
+    {
+      name: "local_convenience_store",
+      value: "e543",
+      turkish: "market1"
+    },
+    {
+      name: "local_dining",
+      value: "e556",
+      turkish: "lokanta"
+    },
+    {
+      name: "local_drink",
+      value: "e544",
+      turkish: "içecek"
+    },
+    {
+      name: "local_fire_department",
+      value: "ef55",
+      turkish: "itfaiye",
+    },
+    {
+      name: "local_florist",
+      value: "e545",
+      turkish: "çiçekçi"
+    },
+    {
+      name: "local_gas_station",
+      value: "e546",
+      turkish: "benzin istasyonu"
+    },
+    {
+      name: "local_grocery_store",
+      value: "e547",
+      turkish: "market2"
+    },
+    {
+      name: "local_hospital",
+      value: "e548",
+      turkish:"hastane"
+    },
+    {
+      name: "local_hotel",
+      value: "e549",
+      turkish:"otel",
+    },
+    {
+      name: "local_laundry_service",
+      value: "e54a",
+      turkish: "çamaşırhane",
+    },
+    {
+      name: "local_library",
+      value: "e54b",
+      turkish: "kütüphane",
+    },
+    {
+      name: "local_mall",
+      value: "e54c",
+      turkish: "avm"
+    },
+    {
+      name: "local_movies",
+      value: "e54d",
+      turkish: "sinema"
+    },
+    {
+      name: "local_offer",
+      value: "e54e",
+      turkish: "alışveriş"
+    },
+    {
+      name: "local_parking",
+      value: "e54f",
+      turkish: "otopark"
+    },
+    {
+      name: "local_pharmacy",
+      value: "e550",
+      turkish: "eczane"
+    },
+    {
+      name: "local_phone",
+      value: "e551",
+      turkish: "telefon",
+    },
+    {
+      name: "local_pizza",
+      value: "e552",
+      turkish: "pizzacı"
+    },
+    {
+      name: "local_police",
+      value: "ef56",
+      turkish: "polis"
+    },
+    {
+      name: "local_post_office",
+      value: "e554",
+      turkish: "posta"
+    },
+    {
+      name: "local_printshop",
+      value: "e555",
+      turkish: "kırtasiye",
+    },
+    {
+      name: "local_see",
+      value: "e557",
+      turkish:"fotoğraf"
+    },
+    {
+      name: "local_shipping",
+      value: "e558",
+      turkish: "taşıma"
+    },
+    {
+      name: "local_taxi",
+      value: "e559",
+      turkish: "taksi"
+    },
+    {
+      name: "sports_soccer",
+      value: "ea29",
+      turkish: "futbol",
+},]
+
+export {priority_icons as default};
